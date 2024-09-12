dd240405551d543db6f6f4d0141b29a018fd0e7b
Vanuck <andrew.jornitz@zoho.com>
2024-09-12 12:10:41 -0700

    Update settings.py

--- recipe_project/settings.py
+++ recipe_project/settings.py
@@ -140,5 +140,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+# db_from_env = dj_database_url.config(conn_max_age=500)
+# DATABASES['default'].update(db_from_env)
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
